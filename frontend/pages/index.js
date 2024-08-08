32e6f0d02fa5afb18bf57ef95c3cbf758fc7a1f3
AK067CSE <akkumar.cse@gmail.com>
2024-08-08 16:01:23 +0530

    Removed spacing from package,json

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import MainView from '../Components/MainView'
+let connected  = true
 export default function Home() {
+  // Assuming `connected` is a state for user authentication status
+  // Initialize with default value (false)
+
   return (
-    <div className={styles.container}>
-      
+    <div className="app">
+      {connected ? (
+        <MainView />
+      ) : (
+        <div className="loginContainer">
+          <div className="loginTitle">Log in to TikTok</div>
+          <div className="loginSubtitle">
+            Manage your account, check notifications, comment on videos, and more
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
